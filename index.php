784a20b03362b290f157a901823d7bc55833be60
ashutoshbhakare <bhakareashutosh@yahoo.com>
2022-03-17 12:14:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,7 @@
 <h1>WebHooks DEMO</h1>
 
 <?php
-echo "Hello World! from GIT";
-echo "Hello from version 3.1 ";
-  echo "FOR KUCL DEMO KUCL IS HOT CAKE !!! ";
-  echo "hello from NOKIA CLASS OF DO180!!!";
+echo "I AM THE FINAL VERSION NOW!!!!"
   
 ?>
 
